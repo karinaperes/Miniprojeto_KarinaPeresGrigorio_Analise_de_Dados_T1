16b629a93d277430e8544a70fd3e89530d4dad02
Karina <karinaperesg@gmail.com>
2026-05-27 14:14:18 -0300

    feat: tratamento de nulos, datas e analise de duplicatas. Arqivos exploracao.py, limpeza.py e main.py

---
+++ src/limpeza.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import unicodedata
+
+def limpar_texto(df):
+    # Remove espaços extras, acentos e converte textos para maiúsculo
+    colunas_texto = df.select_dtypes(include=['object']).columns
+
+    for coluna in colunas_texto:
+
+        # Ignora DATA pois será tratada separadamente
+        if coluna == 'DATA':
+            continue
+
+        df[coluna] = df[coluna].astype(str).str.strip()
+
+        df[coluna] = df[coluna].apply(
+            lambda x: unicodedata.normalize('NFKD', x)
+            .encode('ascii', 'ignore')
+            .decode('utf-8')
+        )
+
+        df[coluna] = df[coluna].str.upper()
+
+    return df
+
+def remover_colunas_vazias(df):
+    # Remove colunas vazias
+    df = df.dropna(axis=1, how='all')    
+    return df
+
+def substituir_nulos(df):
+    #Substitui valores "#N/D" por NaN na coluna PR_CAT
+    df['PR_CAT'] = df['PR_CAT'].replace('#N/D', pd.NA)
+    return df
+
+def limpar_datas(df):
+    # Remove espaços e converte a coluna DATA para datetime
+    df['DATA'] = (
+        df['DATA']
+        .astype(str)
+        .str.strip()
+        .str.replace(r'[^0-9/]', '', regex=True)
+    )
+
+    df['DATA'] = pd.to_datetime(
+        df['DATA'],
+        format='%d/%m/%Y',
+        errors='coerce'
+    )
+    return df
+
+def limpar_duplicatas(df):
+    # Remove linhas duplicadas
+    df = df.drop_duplicates()
+    return df
